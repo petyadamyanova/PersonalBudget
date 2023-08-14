e1517d74999c3203c29fcfeafd605b9ccfe829b3
A <A@A.a>
2023-08-14 09:59:26 +0300

    Initial Commit

---
+++ PersonalBudget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalBudget
+//
+//  Created by Student on 14.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
